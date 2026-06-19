12bb5aed6cb7828f515c3b38b17594a42e4431ca
PizzaeatGaming123 <biuetukatukatuka1226@gmail.com>
2026-06-19 15:34:07 +0900

    feat(backend): add domain enums

---
+++ backend/src/main/java/jp/akiyume/shift/domain/EmploymentType.java
@@ -0,0 +1,19 @@
+package jp.akiyume.shift.domain;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum EmploymentType {
+    FULL_TIME("正社員"),
+    PART_TIME("パート");
+
+    private final String label;
+
+    EmploymentType(String label) {
+        this.label = label;
+    }
+
+    @JsonValue
+    public String getLabel() {
+        return label;
+    }
+}
